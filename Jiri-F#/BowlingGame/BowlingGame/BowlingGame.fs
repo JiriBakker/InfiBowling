b6457309a727ad72f4ee289a100531aa3b6fcf08
Jiri Bakker <jiri.bakker@infi.nl>
2015-07-31 19:28:50 +0200

    Refactoring: attempt to extract mutable state from functions

--- Jiri-F#/BowlingGame/BowlingGame/BowlingGame.fs
+++ Jiri-F#/BowlingGame/BowlingGame/BowlingGame.fs
@@ -5,121 +5,130 @@ open System;
 type InvalidPinCountException(message) =
    inherit Exception(message)
 
-type InvalidFrameNumberExceptoin(message) =
+type InvalidFrameNumberException(message) =
    inherit Exception(message)
 
 type GameNotActiveException(message) =
    inherit Exception(message)
    
 
-type FrameResult = Normal | Spare | Strike
+type private FrameResult = Normal | Spare | Strike
 
-type GameState = ReadyForFirstThrow | ReadyForSecondThrow | GameFinished
+type private GameState = ReadyForFirstThrow | ReadyForSecondThrow | GameFinished
 
 type Game() =
 
-    let frameScores  = new Dictionary<int, int>()
-    let frameResults = new Dictionary<int, FrameResult>()
+    let isValidFrameNr   frameNr = (frameNr >= 1 && frameNr <= 12)
+    let isRegularFrameNr frameNr = (frameNr >= 1 && frameNr <= 10)
+
+    let isFirstThrow gameState = 
+        (gameState = ReadyForFirstThrow)
+
+    let toggleFirstThrow gameState =
+        match gameState with
+            | ReadyForFirstThrow  -> ReadyForSecondThrow
+            | ReadyForSecondThrow -> ReadyForFirstThrow
+            | GameFinished        -> raise (GameNotActiveException("Game has ended"))        
+
+    let assertValidFrameNr frameNr = 
+        if not (isValidFrameNr frameNr) then raise (InvalidFrameNumberException("Frame number should be higher than or equal to 1 and less than or equal to 12"))         
+
+    let assertValidFrameScore frameScore =
+        if frameScore > 10 then raise (InvalidPinCountException("Pin count is higher than number of currently remaining pins in frame"))
+
+    let getFrameScore frameNr frameScores =
+        match Map.tryFind frameNr frameScores with
+        | Some score -> score
+        | None -> 0
+        
+    
+    let getFrameResult frameNr frameResults =
+        match Map.tryFind frameNr frameResults with
+        | Some result -> result
+        | None -> Normal
+
+    let updatePreviousFrameScores frameNr pins frameResults frameScores gameState =        
+        seq {
+            if frameNr > 1 then            
+                let previousResult = getFrameResult (frameNr - 1) frameResults
+
+                if (previousResult = Spare && isFirstThrow gameState) || previousResult = Strike then
+                    yield (frameNr - 1, (getFrameScore(frameNr - 1) frameScores) + pins)
+            
+                if frameNr > 2 then
+                    if (getFrameResult(frameNr - 2) frameResults) = Strike && previousResult = Strike then
+                        yield (frameNr - 2, (getFrameScore(frameNr - 2) frameScores) + pins)    
+        }
+
+    let previousFrameResult currentFrameNr = 
+        getFrameResult (currentFrameNr - 1)    
+
+    let checkIfGameFinished currentFrameNr gameState frameResults =
+        match (currentFrameNr, previousFrameResult currentFrameNr frameResults) with
+            | (13, _)
+            | (12, Normal)
+            | (11, Normal) -> GameFinished
+            | _ -> gameState
+
+    let checkGameIsActive gameState =
+        if gameState = GameFinished then raise (GameNotActiveException("Game has ended"))
+
+    let validatePinCount pins = 
+        if pins < 0 || pins > 10 then raise (InvalidPinCountException("Pin count should be higher than or equal to 0 and less than or equal to 10"))        
+    
+
+
+    let mutable frameScores  = Map.empty
+    let mutable frameResults = Map.empty
 
     let mutable currentFrameNr = 1
 
     let mutable gameState = ReadyForFirstThrow
 
-    let isValidFrameNr   frameNr = (frameNr >= 1 && frameNr <= 12)
-    let isRegularFrameNr frameNr = (frameNr >= 1 && frameNr <= 10)
-
-    let isFirstThrow () = 
-        (gameState = ReadyForFirstThrow)
-
-    let incrementFrameNr () = 
-        currentFrameNr <- currentFrameNr + 1
-
-    let toggleFirstThrow () =
-        match gameState with
-            | ReadyForFirstThrow  -> gameState <- ReadyForSecondThrow
-            | ReadyForSecondThrow -> gameState <- ReadyForFirstThrow
-            | GameFinished        -> raise (GameNotActiveException("Game has ended"))
-
-    let setFrameScore frameNr score =
-        if not (isValidFrameNr frameNr) then raise (InvalidFrameNumberExceptoin("Frame number should be higher than or equal to 1 and less than or equal to 12"))     
-        if isRegularFrameNr frameNr then
-            if not (frameScores.ContainsKey(frameNr)) then frameScores.Add(frameNr, score)
-            else frameScores.Item(frameNr) <- score
-
-    let getFrameScore frameNr =
-        if not(frameScores.ContainsKey(frameNr)) then frameScores.Add(frameNr, 0)
-        frameScores.[frameNr];
+    let setFrameScore frameNr score =       
+        frameScores.Add (frameNr,score)
 
     let setFrameResult frameNr frameResult =
-        if not (isValidFrameNr frameNr) then raise (InvalidFrameNumberExceptoin("Frame number should be higher than or equal to 1 and less than or equal to 12"))     
-        
-        if not (frameResults.ContainsKey(frameNr)) then frameResults.Add(frameNr, frameResult)
-        else frameResults.Item(frameNr) <- frameResult
-
-    let getFrameResult frameNr =
-        if not (frameResults.ContainsKey(frameNr)) then frameResults.Add(frameNr, Normal)
-        frameResults.[frameNr];
-
-    let updatePreviousFrameScores frameNr pins =        
-        if frameNr > 1 then
-            let previousResult = getFrameResult(frameNr - 1)
-
-            if (previousResult = Spare && isFirstThrow()) || previousResult = Strike then
-                setFrameScore (frameNr - 1) (getFrameScore(frameNr - 1) + pins)
-            
-            if frameNr > 2 then
-                if getFrameResult(frameNr - 2) = Strike && previousResult = Strike then
-                    setFrameScore (frameNr - 2) (getFrameScore(frameNr - 2) + pins)    
-
-    let previousFrameResult () = 
-        getFrameResult (currentFrameNr - 1)
-
-    let finishFrame result =
-       setFrameResult currentFrameNr result
-       gameState <- ReadyForFirstThrow
-
-    let checkIfGameFinished () =
-        if not (isValidFrameNr currentFrameNr)
-            || (currentFrameNr = 12 && previousFrameResult() = Normal)
-            || (currentFrameNr = 11 && previousFrameResult() = Normal) then
-            gameState <- GameFinished
-
-    let checkGameIsActive () =
-        if gameState = GameFinished then raise (GameNotActiveException("Game has ended"))
-
-    let validatePinCount pins = 
-        if pins < 0 || pins > 10 then raise (InvalidPinCountException("Pin count should be higher than or equal to 0 and less than or equal to 10"))        
-
-    let validateFrameScore frameScore =
-        if frameScore > 10 then raise (InvalidPinCountException("Pin count is higher than number of currently remaining pins in frame"))
+        frameResults.Add (frameNr,frameResult)
 
     member this.CurrentScore = 
-        Seq.sum(frameScores.Values)
+        frameScores
+        |> Map.toSeq
+        |> Seq.sumBy (fun (k,v) -> v)
 
     member this.Gooi pins =
-        checkGameIsActive()
+        checkGameIsActive gameState
         validatePinCount pins        
 
-        let frameScore = getFrameScore(currentFrameNr) + pins
+        let frameScore = getFrameScore (currentFrameNr + pins) frameScores
+        
+        assertValidFrameScore frameScore
 
-        validateFrameScore frameScore       
+    
+        if isRegularFrameNr currentFrameNr then
+            assertValidFrameNr currentFrameNr
+            frameScores <- setFrameScore currentFrameNr frameScore 
 
-        setFrameScore currentFrameNr frameScore
-        updatePreviousFrameScores currentFrameNr pins
+        for (frameNr,score) in updatePreviousFrameScores currentFrameNr pins frameResults frameScores gameState do
+            frameScores <- setFrameScore frameNr score 
 
-        if frameScore = 10 then 
-            if isFirstThrow() then finishFrame Strike
-            else finishFrame Spare           
-        else 
-            toggleFirstThrow()
 
-        if isFirstThrow() then             
-            incrementFrameNr()
+        gameState <- 
+            match (frameScore, isFirstThrow gameState) with
+            | (10, true) -> 
+                frameResults <- setFrameResult currentFrameNr Strike
+                ReadyForFirstThrow
+            | (10, false) -> 
+                frameResults <- setFrameResult currentFrameNr Spare
+                ReadyForFirstThrow     
+            | _ -> toggleFirstThrow gameState
 
-        checkIfGameFinished()    
+        if isFirstThrow gameState then             
+            currentFrameNr <- currentFrameNr + 1
+
+        gameState <- checkIfGameFinished currentFrameNr gameState frameResults  
 
     member this.ScoreVoorFrame frameNr =
-        let mutable cumulativeScore = 0
-        for i = 1 to frameNr do
-            cumulativeScore <- cumulativeScore + getFrameScore(i)
-        cumulativeScore
+        [1..frameNr]        
+        |> Seq.map (fun (i) -> getFrameScore i frameScores)        
+        |> Seq.sum 
